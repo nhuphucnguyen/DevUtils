4fec9fa9420b6878078793177214fc3ffd981c64
Phuc Nguyen <phucnn.work@gmail.com>
2025-08-21 23:58:51 +0800

    Initial Commit

---
+++ DevUtils/DevUtilsApp.swift
@@ -0,0 +1,17 @@
+//
+//  DevUtilsApp.swift
+//  DevUtils
+//
+//  Created by Nguyen Nhu Phuc on 21/8/25.
+//
+
+import SwiftUI
+
+@main
+struct DevUtilsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
